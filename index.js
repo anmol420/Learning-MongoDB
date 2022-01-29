63fb2ed77ad74fb221927cd434a850c5bb6324eb
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2022-01-29 14:06:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require("./User.js");
 
-mongoose.connect("mongodb://localhost/testdb") //You Can Give Your MongoDB URI Inside "" If You Dont Have MongoDB Locally Installed
+mongoose.connect("mongodb://localhost:27017/testdb") //You Can Give Your MongoDB URI Inside "" If You Dont Have MongoDB Locally Installed
 
 const user = new User({ name : "Mr.X", age : 18 })
 
