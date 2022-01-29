de8735a00c04be19ba09bd37d44e808965aa7039
Anmol Anand <anmolanand2k15@gmail.com>
2022-01-29 14:01:43 +0530

    Added User.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const User = require("./User.js");
 
-mongoose.connect("mongodb://localhost/testdb") //You Can Give Your MongoDB URI Inside The ""
+mongoose.connect("mongodb://localhost/testdb") //You Can Give Your MongoDB URI Inside "" If You Dont Have MongoDB Locally Installed
 
+const user = new User({ name : "Mr.X", age : 18 })
+
+user.save().then(() => console.log("User Saved"))
